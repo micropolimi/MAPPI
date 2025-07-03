6f6d9a609d04845507bf4fc1734189942a97f0ff
GiorgiaTortora <giorgiatortora2@gmail.com>
2025-07-03 14:30:26 +0200

    update code

--- src/PlantMacroscope/plant_app.py
+++ src/PlantMacroscope/plant_app.py
@@ -16,28 +16,24 @@ class camera_app(BaseMicroscopeApp):
         #Add hardware components
         print("Adding Hardware Components")
         from camera_hw import FlirHW
-        self.add_hardware(FlirHW(self, name='camera_y'))
-        self.add_hardware(FlirHW(self, name='camera_x'))
-         
+        
+        cameray = FlirHW(self, name='camera_y')
+        cameray.settings['serial'] = '17289280'
+        self.add_hardware(cameray)
+        
+        camerax = FlirHW(self, name='camera_x')
+        camerax.settings['serial'] = '14103019'
+        self.add_hardware(camerax)
+        
         from io_hw import IoHW
         self.add_hardware(IoHW(self, name='led_y'))
         self.add_hardware(IoHW(self, name='led_x'))
-        
-        
+         
         # Add measurement components
         print("Create Measurement objects")
-        # from Flir_ScopeFoundry.camera_measure import FlirMeasure
-        # self.add_measurement(FlirMeasure(self))
-        
-        #from plant_measure import PlantMeasure
-        #self.add_measurement(PlantMeasure(self))
-        
-        #from plant_timelapse_measure import PlantTimeLapseMeasure
-        #self.add_measurement(PlantTimeLapseMeasure(self))
 
         from plant_timelapse_dual_measure import PlantTimeLapseDualMeasure
         self.add_measurement(PlantTimeLapseDualMeasure(self))
-
 
         self.ui.show()
         self.ui.activateWindow()
@@ -50,6 +46,6 @@ if __name__ == '__main__':
     app.settings_load_ini(".\\Settings\\settings_timelapse_dualview.ini")
     
     for hc_name, hc in app.hardware.items():
-        hc.settings['connected'] = True    # connect all the hardwares  
+          hc.settings['connected'] = True    # connect all the hardwares  
     
     sys.exit(app.exec_())
--- src/PlantMacroscope/plant_timelapse_dual_measure.py
+++ src/PlantMacroscope/plant_timelapse_dual_measure.py
@@ -29,12 +29,14 @@ class PlantTimeLapseDualMeasure(Measurement):
         self.ui_filename = sibling_path(__file__, "camera.ui")
         self.ui = load_qt_ui_file(self.ui_filename)
         self.settings.New('camera_in_use', dtype=str, choices=list(VIEWS), initial=list(VIEWS)[0])
-        self.settings.New('time_lapse_num', dtype=int,
-                          initial=1)
+        self.settings.New('acquisition_time', dtype=float, unit='ms',
+                          initial=100.0, spinbox_decimals=3, vmin=0.01)
+        self.settings.New('time_lapse_num', dtype=int, initial=1, vmin=1)
         self.settings.New('time_lapse_waiting_time', dtype=float, unit='s',
-                          initial=1.0, spinbox_decimals=3)
+                          initial=5.0, spinbox_decimals=3)
+        self.settings.New('frame_num',dtype=int,initial=1,vmin=1)
         self.settings.New('LED_init_time', dtype=float, unit='s',
-                          initial=0.5, spinbox_decimals=3)
+                          initial=0.0, spinbox_decimals=3)
         self.settings.New('xsampling', dtype=float, unit='um',
                           initial=0.058, spinbox_decimals=3)
         self.settings.New('ysampling', dtype=float, unit='um',
@@ -47,9 +49,53 @@ class PlantTimeLapseDualMeasure(Measurement):
         self.settings.New('level_max', dtype=int, initial=4000)
         self.settings.New('save_h5', dtype=bool, initial=False)
         self.settings.New('refresh_period', dtype=float,
-                          unit='s', spinbox_decimals=3, initial=0.05, vmin=0)
-
+                          unit='s', spinbox_decimals=3, initial=0.03, vmin=0)
         
+        self.setup_hardware()
+
+    def set_view(self,view='X'):
+        if hasattr(self, 'cameras'):
+            cam = self.cameras[VIEWS[view]]
+            acq_time = cam.settings['exposure_time']
+            self.settings['acquisition_time'] = acq_time
+    
+    def set_acq_time(self, desired_time):
+        if hasattr(self, 'cameras'):
+            view = self.settings['camera_in_use']
+            cam_idx = VIEWS[view]
+            cam = self.cameras[cam_idx]
+            # maxfr = cam.camera.cam.get_info('AcquisitionFrameRate')['max'] 
+            # minfr = cam.camera.cam.get_info('AcquisitionFrameRate')['min']
+            # desired_rate = 1000/desired_time * 0.95 # consider 5% less than maximum achievable
+            desired_rate = 1.0 #TODO: fix the syncronization bug
+            cam.settings['frame_rate'] = desired_rate
+            # fr = cam.camera.cam.AcquisitionFrameRate
+            maxexp = cam.camera.cam.get_info('ExposureTime')['max']/1000.0
+            cam.settings['exposure_time'] = desired_time
+            if desired_time > maxexp:
+                print(f'Warning: desiderd exposure time {desired_time} exceeds maximun time {maxexp}')
+            
+            
+    def setup_hardware(self):
+        self.cameras = []
+        self.leds = []
+        
+        cam = self.app.hardware['camera_y']
+        self.cameras.append(cam)
+        
+        self.leds.append(self.app.hardware['led_y'])
+        self.app.hardware['led_y'].channel_1.connect_to_widget(self.ui.led_y_Box)
+        
+        cam = self.app.hardware['camera_x']
+        self.cameras.append(cam)
+        
+        self.leds.append(self.app.hardware['led_x'])   
+        self.app.hardware['led_x'].channel_1.connect_to_widget(self.ui.led_x_Box)
+        
+        for c in self.cameras:
+            c.read_from_hardware()
+            
+            
     def setup_figure(self):
         """
         Runs once during App initialization, after setup()
@@ -66,7 +112,13 @@ class PlantTimeLapseDualMeasure(Measurement):
         self.settings.auto_range.connect_to_widget(self.ui.autoRange_checkbox)
         self.settings.level_min.connect_to_widget(self.ui.min_doubleSpinBox)
         self.settings.level_max.connect_to_widget(self.ui.max_doubleSpinBox)
-
+        self.settings.time_lapse_num.connect_to_widget(self.ui.num_Box)
+        self.settings.time_lapse_waiting_time.connect_to_widget(self.ui.waiting_timeBox)
+        self.settings.camera_in_use.connect_to_widget(self.ui.viewBox)
+        self.settings.acquisition_time.connect_to_widget(self.ui.acquisitionBox)
+        self.settings.camera_in_use.hardware_set_func = self.set_view
+        self.settings.acquisition_time.hardware_set_func = self.set_acq_time
+        
         # Set up pyqtgraph graph_layout in the UI
         self.imv = pg.ImageView()
         self.ui.imageLayout.addWidget(self.imv)
@@ -86,7 +138,7 @@ class PlantTimeLapseDualMeasure(Measurement):
         This function runs repeatedly and automatically during the measurement run.
         its update frequency is defined by self.display_update_period
         """
-        #self.display_update_period = self.settings['refresh_period']
+        self.display_update_period = self.settings['refresh_period']
         length = self.settings['time_lapse_num']
 
         self.settings['progress'] = (self.time_index + 1) * 100/length
@@ -114,11 +166,13 @@ class PlantTimeLapseDualMeasure(Measurement):
         
         time_lapse_num = self.settings['time_lapse_num'] 
         time_lapse_waiting_time = self.settings['time_lapse_waiting_time']
-        frame_num = self.cameras[0].frame_num.val
+        frame_num = self.settings.frame_num.val
+        
         for c in self.cameras:
-            c.camera.acq_stop()
+            #c.camera.acq_stop()
             c.settings['acquisition_mode'] = 'MultiFrame'
-            c.camera.set_framenum(frame_num)
+            c.settings['frame_num'] = frame_num+1 #note that we acquire and than skip the first frame
+            c.read_from_hardware()
         
         self.initial_time = time.time()
         
@@ -129,24 +183,26 @@ class PlantTimeLapseDualMeasure(Measurement):
             
             for L in self.leds:
                 L.turn_on()
+                L.read_from_hardware()
             time.sleep(self.settings['LED_init_time']) # waiting time to turn on the LED
             first_frame_acquired = False
             for c in self.cameras:
                 c.camera.acq_start()
+                
+            self.images = []
             
-            for frame_idx in range(frame_num):
+            for frame_idx in range(frame_num+1): #note that we acquire and than skip the first frame
                 
                 self.frame_index = frame_idx
-                
                 for cam_idx,cam in enumerate(self.cameras):
-                    img = cam.camera.get_nparray()
+                    img = cam.camera.get_nparray() #TODO: syncro bug make sure that the image has been acquired before getting it
+                    # print('acq time:', time.time() - self.initial_time)
                     if first_frame_acquired:
                         self.images[cam_idx] = img
                     else:
-                        self.images.append(img)
-                    if c == self.cameras[VIEWS[self.settings['camera_in_use']]]:
-                        self.img = img
-                                
+                        self.images.append(img) 
+                    if cam == self.cameras[VIEWS[self.settings['camera_in_use']]]:
+                        self.img = img             
                 if self.settings['save_h5']:
                     if not first_frame_acquired:
                         if time_lapse_idx == 0:
@@ -154,19 +210,20 @@ class PlantTimeLapseDualMeasure(Measurement):
                         else:
                             self.init_h5_dataset(time_lapse_idx)
                         first_frame_acquired = True
-    
                     for cam_idx,cam in enumerate(self.cameras):
-                        self.h5_datasets[cam_idx][frame_idx, :, :] = self.images[cam_idx]
-                    
+                        self.h5_datasets[cam_idx][frame_idx-1, :, :] = self.images[cam_idx] # #note that we acquire and than skip the first frame
+               
                     self.h5file.flush()
+
                 if self.interrupt_measurement_called:
                     break
-
+            
             for c in self.cameras:
                 c.camera.acq_stop()
             for L in self.leds:
                 L.turn_off()
-            # wait for next time lapse measurevment
+                L.read_from_hardware()
+            # wait for next time lapse measurement
             while time.time()<self.initial_time+(time_lapse_idx+1)*time_lapse_waiting_time:
                 if self.interrupt_measurement_called:
                     break
@@ -178,20 +235,12 @@ class PlantTimeLapseDualMeasure(Measurement):
         focus on data acquisition.
         
         """
-        self.cameras = []
-        self.leds = []
-        if self.app.hardware['camera_y'].connected:
-            self.cameras.append(self.app.hardware['camera_y'])
-        if self.app.hardware['led_y'].connected:
-            self.leds.append(self.app.hardware['led_y'])
-        
-        if self.app.hardware['camera_x'].connected:
-             self.cameras.append(self.app.hardware['camera_x'])
-        if self.app.hardware['led_x'].connected:
-            self.leds.append(self.app.hardware['led_x'])        
         
         for c in self.cameras:
             c.read_from_hardware()
+            
+        self.settings['camera_in_use'] = 'X'
+        self.settings['camera_in_use'] = 'Y'
 
         try:
             self.frame_index = -1
@@ -209,6 +258,8 @@ class PlantTimeLapseDualMeasure(Measurement):
 
                 if self.settings['save_h5']:
                     # measure is triggered by save_h5 button
+                    for c in self.cameras:
+                        c.camera.acq_stop()
                     self.measure()
                     break
 
@@ -247,16 +298,15 @@ class PlantTimeLapseDualMeasure(Measurement):
         img_size = self.img.shape
         dtype = self.img.dtype
         actual_time = time.time()-self.initial_time
-        print('measurement:', time_idx, 'at time:', actual_time )
-        length = self.cameras[0].frame_num.val
+        print('measurement:', time_idx, 'at time:', actual_time)
+        length = self.settings.frame_num.val
         self.h5_datasets = []
         for c_idx,c in enumerate(self.cameras):
             name = f't{time_idx:04d}/c{c_idx}/image'
             dataset = self.h5_group.create_dataset(name=name,
-                                                    shape=[
-                                                    length, img_size[0], img_size[1]],
+                                                    shape=[length, img_size[0], img_size[1]],
                                                     dtype=dtype)
-            dataset.attrs['view'] = VIEWS[c_idx]
+            dataset.attrs['view'] = c_idx
             dataset.attrs['time_idx'] = time_idx
             dataset.attrs['acquisition_time'] = actual_time
             dataset.attrs['frame_interval_s'] = self.settings['time_lapse_waiting_time']
